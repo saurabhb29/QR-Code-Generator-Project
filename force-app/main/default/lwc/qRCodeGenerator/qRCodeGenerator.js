02099b843941393f8b4cc5401dc0783e3f9f6665
Saurabh Bisht <1999saurabhbisht@gmail.com>
2024-01-02 15:55:49 +0530

    Neq Update

--- force-app/main/default/lwc/qRCodeGenerator/qRCodeGenerator.js
+++ force-app/main/default/lwc/qRCodeGenerator/qRCodeGenerator.js
@@ -9,13 +9,23 @@ export default class QRCodeGenerator extends LightningElement {
 
     inputChange(event) {
         this.inputValue = event.target.value;
+        // Check if input value is blank
+        if (!this.inputValue.trim()) {
+            this.showQRCode = false;
+        }
 
         
     }
 
     letterHandler() {
-        this.showQRCode      = true;
-        this.qrCodeImageUrl    = this.url + this.inputValue;
+        // this.showQRCode      = true;
+        // this.qrCodeImageUrl    = this.url + this.inputValue;
+        if (this.inputValue.trim() !== '') {
+            this.showQRCode = true;
+            this.qrCodeImageUrl = this.url + this.inputValue;
+        } else {
+            this.showQRCode = false;
+        }
 
     }
 }
